255dede1192c5d8c1a0f88c53793312d1a746d05
Jacob Stafford <staffjacob@live.com>
2017-02-28 17:24:25 +0000

    User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+    
+    def new
+      #needs user id, last name, first name look at db
+      @user = User.new
+
+    end
+    
+    def create
+    end
+    
+    def index
+      @users = User.all
+    end
+    
 end

97c3fb212f4f95d390c413b53e0c74d598e3c24f
Cody Leuschner <cidythegreat@tamu.edu>
2017-03-06 00:17:17 +0000

    Add users controller unit tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "creating a new user should save to database and generate an id" do
+      testFirstName = "first"
+      testLastName = "last"
+      User.create(testFirstName, testLastName)
+      newUser = User.find_by(firstName: testFirstName, lastName: testLastName)
+      assert newUser != nil && newUser.userId != nil
+   end
+   
+   test "creating a new user should have a first name" do
+      testFirstName = "first"
+      testLastName = "last"
+      User.create(testFirstName, testLastName)
+      newUser = User.find_by(firstName: testFirstName, lastName: testLastName)
+      assert newUser != nil && newUser.userId != nil
+      assert newUser.firstName == testFirstName
+   end
+   
+   test "creating a new user should have a last name" do
+      testFirstName = "first"
+      testLastName = "last"
+      User.create(testFirstName, testLastName)
+      newUser = User.find_by(firstName: testFirstName, lastName: testLastName)
+      assert newUser != nil && newUser.userId != nil
+      assert newUser.lastName == testLastName
+   end
+   
+   test "creating a new user should set total points to o" do
+      testFirstName = "first"
+      testLastName = "last"
+      User.create(testFirstName, testLastName)
+      newUser = User.find_by(firstName: testFirstName, lastName: testLastName)
+      assert newUser != nil && newUser.userId != nil
+      assert newUser.totalPoints == 0
+   end
 end

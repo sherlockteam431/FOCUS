65c5c372ed31e6132dfe712825d4ae39ed247282
Jacob Stafford <staffjacob@live.com>
2017-03-07 16:32:15 +0000

    UserController in Progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,26 @@
 class UsersController < ApplicationController
     
     def new
-      #needs user id, last name, first name look at db
-      @user = User.new
 
     end
     
     def create
+      #needs user id, last name, first name look at db
+      #random user id 
+      # first and last name
+      uniqId = false
+      while !uniqId do
+        newUserId = Random.rand(9999)
+        puts newUserId
+        #check if client userId alreaady exists. If it does generate another
+        check = User.where(["userId = ?", newUserId])
+        if(check == nil || check == [])
+          uniqId = true
+        end
+      end
+      first = "Jacob"
+      last  = "Stafford"
+      @user = User.create!(firstName: first, lastName: last, userId: newUserId)
     end
     
     def index
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'users_controller.rb'
 
 class UsersControllerTest < ActionController::TestCase
    test "creating a new user should save to database and generate an id" do
@@ -6,8 +7,17 @@ class UsersControllerTest < ActionController::TestCase
       testLastName = "last"
       User.create(testFirstName, testLastName)
       newUser = User.find_by(firstName: testFirstName, lastName: testLastName)
-      assert newUser != nil && newUser.userId != nil
+      assert (newUser != nil && newUser.userId != nil)
    end
+   
+   
+   #test "the truth" do
+   test "create with post" do
+      user_params = {firstName: 'first', lastName: 'last', userId: 21}
+      post :create , :user => user_params
+      assert_response :success
+   end
+   #end
    
    test "creating a new user should have a first name" do
       testFirstName = "first"
@@ -21,16 +31,16 @@ class UsersControllerTest < ActionController::TestCase
    test "creating a new user should have a last name" do
       testFirstName = "first"
       testLastName = "last"
-      User.create(testFirstName, testLastName)
+      #create(testFirstName, testLastName)
       newUser = User.find_by(firstName: testFirstName, lastName: testLastName)
       assert newUser != nil && newUser.userId != nil
       assert newUser.lastName == testLastName
    end
    
-   test "creating a new user should set total points to o" do
+   test "creating a new user should set total points to 0" do
       testFirstName = "first"
       testLastName = "last"
-      User.create(testFirstName, testLastName)
+      #create(testFirstName, testLastName)
       newUser = User.find_by(firstName: testFirstName, lastName: testLastName)
       assert newUser != nil && newUser.userId != nil
       assert newUser.totalPoints == 0

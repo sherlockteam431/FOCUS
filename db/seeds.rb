11219407268617e2c006e6a55f4266dce24b83e9
Cody Leuschner <cidythegreat@tamu.edu>
2017-03-29 23:35:50 +0000

    Update seed data to reflect events and user changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 users = [
-        {:userId => '0001', :firstName => 'Cody', :lastName => 'Leuschner', :totalPoints => '97', :role => "admin"},
-        {:userId => '0002', :firstName => 'Nicholas', :lastName => 'Maxwell', :totalPoints => '97', :role => "admin"},
-        {:userId => '0003', :firstName => 'Victor', :lastName => 'Martinez', :totalPoints => '97', :role => "admin"},
-        {:userId => '0004', :firstName => 'Jacob', :lastName => 'Stafford', :totalPoints => '97', :role => "admin"},
-        {:userId => '0005', :firstName => 'Josh', :lastName => 'Hooton', :totalPoints => '97', :role => "admin"},
-        {:userId => '0006', :firstName => 'Jason', :lastName => 'Nguyen', :totalPoints => '97', :role => "admin"},
+        {:userId => '0001', :firstName => 'Cody', :lastName => 'Leuschner', :role => "admin"},
+        {:userId => '0002', :firstName => 'Nicholas', :lastName => 'Maxwell', :role => "admin"},
+        {:userId => '0003', :firstName => 'Victor', :lastName => 'Martinez', :role => "admin"},
+        {:userId => '0004', :firstName => 'Jacob', :lastName => 'Stafford', :role => "admin"},
+        {:userId => '0005', :firstName => 'Josh', :lastName => 'Hooton', :role => "admin"},
+        {:userId => '0006', :firstName => 'Jason', :lastName => 'Nguyen', :role => "admin"},
     ]
     
 users.each do |user|
@@ -20,8 +20,8 @@ User.create!(user)
 
 end
 
-events = [{:name => 'Dummy 1', :points => '10'},
-          {:name => 'Dummy 2', :points => '20'},
+events = [{:name => 'Dummy 1', :points => '10', :mentors => '0001, 0002, 0003', :date => DateTime.new(2017, 3, 1, 12), :eventType => 'Other', :description => "This is a test"},
+          {:name => 'Dummy 2', :points => '20', :mentors => '0002, 0004, 0006', :date => DateTime.new(2017, 3, 3, 12), :eventType => 'Other', :description => "This is only a test"},
       ]
       
 events.each do |event|

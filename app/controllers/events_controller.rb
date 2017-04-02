9b342a8dd5e0ae6d89cf6c417f48f14b9c84b737
Jacob Stafford <staffjacob@live.com>
2017-04-02 00:05:12 +0000

    Front end for event creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,14 +5,23 @@ class EventsController < ApplicationController
     end
     
     def create
-      name = params[:event][:name]
-      points =  params[:event][:points]
-      if(name == "" || points == "")
-        flash[:message] = "Must have a name and points value (can be 0)." 
-        redirect_to :controller => "events", :action => :new
-        return
+      for obj in params[:event] do
+        if(obj == nil || obj == "")
+          flash[:message] = "Must fill in #{obj.to_s} to complete the form!" 
+          redirect_to :controller => "events", :action => :new
+          return
+        end
       end
-      @event = Event.create!(name: name, points: points)
+      
+      name        = params[:event][:name]
+      points      = params[:event][:points]
+      type        = params[:event][:eventType]
+      description = params[:event][:description]
+      mentors     = params[:event][:mentors]
+      date        = params[:event][:date] 
+      
+      
+      @event = Event.create!(name: name, points: points, eventType: type, description: description, mentors: mentors, date: date)
       redirect_to :controller => "events", :action => :show
     end
     
--- config/routes.rb
+++ config/routes.rb
@@ -18,10 +18,13 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  
+  
 
   
   
   resources :users
+  resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

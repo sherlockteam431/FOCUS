4488ea3b55a949b06e31b8ecf01b84e027d010f1
Jason Nguyen <pnguyen59@tamu.edu>
2017-04-17 12:45:58 +0000

    fix create new user in admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,7 +3,33 @@ ActiveAdmin.register User do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
     permit_params :firstName, :lastName, :userId
-#
+    
+    
+    controller do
+        def create
+            @user = User.new(permitted_params[:user])
+            
+        uniqId = false
+         #check if client userId already exists. If it does generate another id
+        while !uniqId do
+        newUserId = Random.rand(9999)
+        check = User.where(userId: newUserId)
+        if(check == nil || check == [])
+          uniqId = true
+        end
+        end
+        @user.userId = newUserId;
+        create!
+        end
+    end
+    
+    form title: "Create new user" do |f|
+        inputs 'details' do
+            f.input :firstName
+            f.input :lastName
+        end
+        f.actions
+    end
 # or
 #
 # permit_params do

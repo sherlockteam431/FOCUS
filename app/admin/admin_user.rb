edefb0a0e3d36ae8491dfb0ffefee1d7eb1c7e56
Cody Leuschner <cidythegreat@tamu.edu>
2017-04-24 00:08:09 +0000

    Clean up admin page

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -3,22 +3,18 @@ ActiveAdmin.register AdminUser do
 
   index do
     selectable_column
-    id_column
     column :email
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
+    column :organization
     actions
   end
 
   filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
+  filter :organization
 
   form do |f|
     f.inputs "Admin Details" do
       f.input :email
+      f.input :organization
       f.input :password
       f.input :password_confirmation
     end
